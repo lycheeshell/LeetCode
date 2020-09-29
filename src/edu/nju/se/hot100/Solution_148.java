8b7e39ca7c54622ab9c1ac08d089763393614c9a
lycheeshell <lycheeshell@163.com>
2020-09-29 14:12:25 +0800

    20200929

---
+++ src/edu/nju/se/hot100/Solution_148.java
@@ -0,0 +1,56 @@
+package edu.nju.se.hot100;
+
+/**
+ * 排序链表
+ * 
+ * @author lycheeshell
+ * @Date 2020-09-29
+ */
+public class Solution_148 {
+
+	public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode fast = head.next, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode tmp = slow.next;
+        slow.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(tmp);
+        ListNode h = new ListNode(0);
+        ListNode res = h;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                h.next = left;
+                left = left.next;
+            } else {
+                h.next = right;
+                right = right.next;
+            }
+            h = h.next;
+        }
+        h.next = left != null ? left : right;
+        return res.next;
+    }
+
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+}
